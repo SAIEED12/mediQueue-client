322a1ccb55c2865897e315702dfdfdae2803f503
SAIEED12 <abusaieed0@gmail.com>
2026-05-21 16:56:43 +0600

    feat: Add framer-motion and lenis for enhanced animations and smooth scrolling; update Providers component

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,9 +1,21 @@
 "use client";
+import { ReactLenis } from "lenis/react";
 import { ThemeProvider } from "next-themes";
+
 export function Providers({ children }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light">
-      {children}
-    </ThemeProvider>
+    <ReactLenis 
+      root 
+      options={{
+        duration: 1.2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
+        smoothWheel: true,
+        orientation: "vertical",
+      }}
+    >
+      <ThemeProvider attribute="class" defaultTheme="light">
+        {children}
+      </ThemeProvider>
+    </ReactLenis>
   );
 }
